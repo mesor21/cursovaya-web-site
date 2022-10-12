970e127a14d2cb345b35934474f159869f1ef8c4
mesor21 <menzdd21@gmail.com>
2022-10-12 19:44:34 +0300

    71 days left

--- file_of_progect/Kursovaya/src/main/java/com/example/Kursovaya/KursovayaApplication.java
+++ file_of_progect/Kursovaya/src/main/java/com/example/Kursovaya/KursovayaApplication.java
@@ -1,12 +1,25 @@
 package com.example.Kursovaya;
 
+import com.example.Kursovaya.person.Chelovek;
+import com.example.Kursovaya.person.Repository.ChelovekRep;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KursovayaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KursovayaApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner demo(ChelovekRep repository){
+		return args -> {
+			repository.save(new Chelovek("Dima","M","123@123.com","234","admin"));
+
+			Chelovek user = repository.findByEmail("123@123.com");
+			System.out.println(user.getName()+" "+user.getSurename());
+		};
 	}
 }
 
--- file_of_progect/Kursovaya/src/main/java/com/example/Kursovaya/person/Repository/ChelovekRep.java
+++ file_of_progect/Kursovaya/src/main/java/com/example/Kursovaya/person/Repository/ChelovekRep.java
@@ -3,5 +3,9 @@ package com.example.Kursovaya.person.Repository;
 import com.example.Kursovaya.person.Chelovek;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChelovekRep extends JpaRepository<Chelovek, Long> {
+    Chelovek findByEmail(String email);
+    Chelovek findById(long id);
 }
